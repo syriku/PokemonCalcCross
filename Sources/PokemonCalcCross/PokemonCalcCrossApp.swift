3c5c2ab6ec2407b955edcc20c6148aa9098d30ee
SHaRIn <syriku@163.com>
2026-02-28 18:30:00 +0800

    Use enum for view routes

--- Sources/PokemonCalcCross/PokemonCalcCrossApp.swift
+++ Sources/PokemonCalcCross/PokemonCalcCrossApp.swift
@@ -4,7 +4,12 @@ import SwiftCrossUI
 
 @main
 struct PokemonCalcCrossApp: App {
-    let viewsKeys = ["Type Calculator", "Type Predictor"]
+    enum ViewRoute: String, CaseIterable {
+        case typeCalculator = "Type Calculator"
+        case typePredictor = "Type Predictor"
+    }
+
+    let viewsKeys = ViewRoute.allCases.map { $0.rawValue }
     @State var selectedViewKey: String?
 
     init() {
@@ -18,14 +23,14 @@ struct PokemonCalcCrossApp: App {
                     .pickerStyle(.segmented)
                     .padding(.top, 5)
 
-                if let selectedViewKey = selectedViewKey {
-                    switch selectedViewKey {
-                    case "Type Calculator":
+                if let selectedViewKey = selectedViewKey,
+                    let route = ViewRoute(rawValue: selectedViewKey)
+                {
+                    switch route {
+                    case .typeCalculator:
                         TypeCalculatorView()
-                    case "Type Predictor":
+                    case .typePredictor:
                         TypePredictorView()
-                    default:
-                        Text("Unknown view")
                     }
                 } else {
                     Text("Please select a view")
